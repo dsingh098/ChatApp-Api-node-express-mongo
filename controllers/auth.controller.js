07f024e1d30aedefd240b15d0c1b07b61c6f224b
deepak-dev23 <ds1090867@gmail.com>
2026-03-24 11:43:13 +0530

    Update auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,13 +3,16 @@ const jwt = require('jsonwebtoken')
 const generateToken = require("../config/token.js")
 const bcrypt = require('bcryptjs')
 
+
+// function/controller for user registration
+
 const register =  async (req, res) => {
  try {
     const {name, email, password, username} = req.body
 
     if(!username || !email || !password) {
         return res.status(400).json({
-            messsage:"All fields are required"
+            message:"All fields are required"
         })
     }
 
@@ -42,7 +45,10 @@ const register =  async (req, res) => {
 
     return res.status(201).json({
         message:"User registered successfully",
-        user: { id: user._id, name: user.name, email: user.email, username: user.username},
+        user: { id: user._id, 
+            name: user.name, email: 
+            user.email, username: 
+            user.username},
         token
     })
     
@@ -52,4 +58,67 @@ const register =  async (req, res) => {
         error:error.message
     })
  }
+}
+
+// function/controller for user login
+
+const login = async (req, res) => {
+    try {
+        const {email,password} = req.body
+
+        if(!email ||!password) {
+            return res.status(400).json({
+                message:"All fields are required"
+            })
+        }
+
+        const existUser = await User.findOne({email})
+
+        if(!existUser) {
+            return res.status(400).json({
+                message:"Invalid credentials"})
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, existUser.password)
+
+        if(!isPasswordValid) {
+            return res.status(400).json({
+                message:"Invalid credentials"
+            })
+        }
+
+        const token = generateToken(existUser)
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "strict",
+            maxAge: 24 * 60 * 60 * 1000
+        })
+
+        return res.status(200).json({
+            message:"Login successful",
+            user: { id: existUser._id, name: existUser.name, email: existUser.email, username: existUser.username},
+            token
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:"Internal Server Error",
+            error:error.message
+        })
+    }
+}
+
+const logout = async (req, res) => {
+    res.clearCookie("token")
+    return res.status(200).json({
+        message:"Logout successful"
+    })
+}
+
+
+module.exports = {
+    register,
+    login,
+    logout
 }
